92679fdc3026c88d02b56e137706db27665f31d7
jr <1932933132@qq.com>
2022-10-07 20:29:17 +0800

    对账统计

---
+++ src/views/report/compontents/marketData.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="marketData">销售信息</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/report/index.vue
+++ src/views/report/index.vue
@@ -1,29 +1,23 @@
 <template>
-  <div>
-    对账统计
+  <div class="report">
+    <day />
+    <el-card class="box-card">
+      <market-data />
+    </el-card>
   </div>
 </template>
 
 <script>
+import day from './compontents/day.vue'
+import MarketData from './compontents/marketData.vue'
 export default {
   name: 'Report',
-
-  data() {
-    return {
-
-    }
-  },
-
-  mounted() {
-
-  },
-
-  methods: {
-
-  }
+  components: { day, MarketData }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.box-card{
+  margin-top: 20px;
+}
 </style>
